3d35905a5c5df7dae62f413b2dd44f03bde50128
bakozetra <synthia.han@onja.org>
2022-05-13 14:05:35 +0300

    ading edit and delete

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,42 +4,9 @@ import Box from '@mui/material/Box';
 import { getCountryFullInfo, allCountries, euCountries, euCandidateCountries } from '../API/index'
 import { DataGrid } from '@mui/x-data-grid'
 import { Switch, FormGroup, FormControlLabel } from '@mui/material';
-
-const columns = [
-  {
-    field: 'countryName',
-    headerName: 'Country Name',
-    type: 'string',
-    width: 300,
-  },
-  {
-    field: 'capital',
-    headerName: 'capital',
-    type: 'string',
-    width: 300,
-  },
-  {
-    field: 'countryPopulation',
-    headerName: 'Country Population',
-    type: 'number',
-    width: 300,
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    field: 'capitalPopulation',
-    headerName: 'Capital Population',
-    type: 'number',
-    width: 300,
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    field: 'capitalPopulationPercentage',
-    headerName: 'Population % live in Capital',
-    type: 'number',
-    width: 300,
-    format: (value) => value.toLocaleString('en-US'),
-  },
-];
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import NestedModal from './modalPopup'
 
 const columnsCountries = [
   {
@@ -55,17 +22,11 @@ const columnsCountries = [
     width: 300,
     format: (value) => value.toLocaleString('en-US'),
   },
-  {
-    field: 'capitalPopulationPercentage',
-    headerName: 'Population % live in Capital',
-    type: 'number',
-    width: 300,
-    format: (value) => value.toLocaleString('en-US'),
-  },
+ 
 
 ]
 
-const columnsCapitalCountries = [
+const columnsCapital = [
   {
     field: 'capital',
     headerName: 'capital',
@@ -79,22 +40,48 @@ const columnsCapitalCountries = [
     width: 300,
     format: (value) => value.toLocaleString('en-US'),
   },
-  {
-    field: 'capitalPopulationPercentage',
-    headerName: 'Population % live in Capital',
-    type: 'number',
-    width: 300,
-    format: (value) => value.toLocaleString('en-US'),
-  },
+
 ]
+
+const defaultColums = (handlEdit, handDelete) =>  [{
+  field: 'capitalPopulationPercentage',
+  headerName: 'Population % live in Capital',
+  type: 'number',
+  width: 300,
+  format: (value) => value.toLocaleString('en-US'),
+}, 
+{
+  field: 'id',
+  headerName: 'Edit and delete',
+
+  type: 'number',
+  width: 300,
+  format: (value) => value.toLocaleString('en-US'),
+  renderCell: (params) => {
+    console.log('params::::::',params.id);
+    return (
+      <Box sx={{ display: 'flex', gap: 4 }}>
+        <ModeEditIcon onClick={() => handlEdit(params.id)} />
+        <DeleteIcon onClick={() => handDelete(params.id)} />
+      </Box>
+    )
+  }
+},]
+function getColums(showCountry, showCapital,handlEdit, handDelete) {
+  const countryCol = showCountry? columnsCountries:[]
+  const capitalCol =showCapital?columnsCapital:[]
+  return (countryCol).concat(capitalCol).concat(defaultColums(handlEdit, handDelete))
+}
 
 export default function StickyHeadTable() {
   const [countriesData, setCountriesData] = useState([])
   const [seachQuery, setSearchQuery] = useState('')
-  const [showCapital, setShowCapital] = useState(false)
-  const [showCountry, setShowCountry] = useState(false)
+  const [showCapital, setShowCapital] = useState(true)
+  const [showCountry, setShowCountry] = useState(true)
+  const [open, setOpen] = React.useState(false);
+  // const [editNumberPopulation , setEditNumberPopulation] = useState([])
   const rows = countriesData?.map(country => {
-    const capitalPopulation =  country.capitalData.population * 100 ;
+    const capitalPopulation = country.capitalData.population * 100;
     const countryPopulation = country.countryData.population * 1000
     const capitalToCountryPopulationPercent = capitalPopulation / countryPopulation
     let roundPercentage = Math.round(capitalToCountryPopulationPercent)
@@ -103,8 +90,8 @@ export default function StickyHeadTable() {
       countryName: country.countryName,
       countryPopulation: country.countryData.population * 1000,
       capital: country.countryData.capital,
-      capitalPopulation: country.capitalData.population, 
-      capitalPopulationPercentage : roundPercentage += '%'
+      capitalPopulation: country.capitalData.population,
+      capitalPopulationPercentage: roundPercentage += '%',
     }
   })
 
@@ -119,47 +106,62 @@ export default function StickyHeadTable() {
     return acc + populationTotal
   }, 0)
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const filterRows = rows?.filter(name => {
     const searchByNameCountry = name?.countryName.toLowerCase().includes(seachQuery.toLowerCase())
-    //  const searchByNameCapital = name?.countryData?.capital.toLowerCase().includes(seachQuery.toLowerCase())
     return searchByNameCountry
   })
+
   useEffect(() => {
     getTableData()
   }, [])
 
-  function showColums() {
-    if (!showCountry && !showCapital) {
-      return columns
-    }
-    if (showCapital) {
-      return columnsCapitalCountries
-    }
-    if (showCountry) {
-      return columnsCountries
-    }
+  const handDelete = (id) => {
+    const fiterPerRow = countriesData?.filter(countryId => countryId.countryName !== id)
+    setCountriesData(fiterPerRow) 
   }
-  
+
+  const handlEdit = (id) => {
+    const fiterPerRow = countriesData?.find(countryId => countryId.countryName === id)
+    if(fiterPerRow) {
+      setOpen(true);
+    }
+    // if(fiterPerRow) {
+    //   return (
+    //     <NestedModal handleOpen={handleOpen} handleClose={handleClose}/>
+    //   )
+    // }
+  }
+
+  function showColums() {
+    return getColums(showCountry, showCapital, handlEdit ,handDelete)
+  }
   return (
     <>
       <Box sx={{ paddingTop: 5, width: '90%', margin: 'auto' }}>
         <SearchBar onChange={(e) => setSearchQuery(e.target.value)} />
       </Box>
-      <div style={{ 
-        width: '90%', 
+      <div style={{
+        width: '90%',
         margin: 'auto',
-        display:'flex',
-        flexDirection:'row', 
-        justifyContent:'space-between',
-        alignItems:'end'
-        
-        }}>
-        <Box sx={{ width: '50%'}}>
-          <p>{`EU countries: ${euCountries.toString()}`}</p>
-          <p>{`Candidate countries: ${euCandidateCountries.toString()}`} </p>
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'end'
+
+      }}>
+        <Box sx={{ width: '50%' }}>
+          <p>{`EU countries: ${euCountries.toString().replace(/,/g, ', ')}`}</p>
+          <p>{`Candidate countries: ${euCandidateCountries.toString().replace(/,/g, ', ')}`} </p>
           <p>{`Total population EU countries combined : ${euCombinedPopulations.toLocaleString('en-US')}`}</p>
         </Box>
-        <FormGroup sx={{display:'flex' , flexDirection:'row' }}>
+        <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
           <FormControlLabel control={<Switch
             checked={showCapital}
             onChange={() => setShowCapital(!showCapital)}
